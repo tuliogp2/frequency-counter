b83019e8fe08947353e513f45ddef1b8a3525872
Tulio Goulart <tulio.gp2@gmail.com>
2021-07-13 18:24:05 +0200

    contador pronto

---
+++ script.js
@@ -0,0 +1,52 @@
+
+const button = document.getElementById("countButton");
+button.addEventListener("click", function () {
+
+    let typedText = document.getElementById("textInput").value;
+
+    typedText = typedText.toLowerCase();
+    typedText = typedText.replace(/[^a-z'\s]+/g, "");
+    words = typedText.split(/\s/);
+
+    const letterCounts = {};
+
+    for (let i = 0; i < typedText.length; i++) {
+        currentLetter = typedText[i];
+
+        if (letterCounts[currentLetter] === undefined) {
+            letterCounts[currentLetter] = 1;
+        }
+        else {
+            letterCounts[currentLetter]++;
+        }
+    }
+
+    for (let letter in letterCounts) {
+        const span = document.createElement("span");
+        const textContent = `"${letter}" : ${letterCounts[letter]}, `;
+        span.innerText = textContent;
+        const letters = document.getElementById("lettersDiv");
+        letters.appendChild(span);
+    }
+
+    const wordCounts = {};
+
+    for (let i = 0; i < words.length; i++) {
+        currentWord = words[i];
+
+        if (wordCounts[currentWord] === undefined) {
+            wordCounts[currentWord] = 1;
+        }
+        else {
+            wordCounts[currentWord]++;
+        }
+    }
+
+    for (let word in wordCounts) {
+        const span = document.createElement("span");
+        const textContent = `"${word}" : ${wordCounts[word]}, `;
+        span.innerText = textContent;
+        const cWords = document.getElementById("wordsDiv");
+        cWords.appendChild(span);
+    }
+});
